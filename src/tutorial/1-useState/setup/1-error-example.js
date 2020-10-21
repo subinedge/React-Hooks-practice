bbcdbbba070c8d0f4ccfb8b21ab7339d6e0f07f3
Subin Sudhakaran-Project SAPPER::Accenture <acc032@GMMCHENNAI.COM>
2020-10-21 20:30:40 +0530

    counter tutorial with useState React

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react'
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
-};
+  const [title, setTitle] = useState('Tutorial')
+  const handleClick = () => {
+    setTitle('You are changing the state')
+  }
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button onClick={handleClick} className="btn">
+        Click this button
+      </button>
+    </React.Fragment>
+  )
+}
 
-export default ErrorExample;
+export default ErrorExample
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
-};
+  const [counter, setCounter] = useState(0)
 
-export default UseStateCounter;
+  const increaseCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const resetCounter = () => {
+    setCounter(0)
+  }
+
+  const decreaseCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    } else {
+      setCounter(0)
+    }
+  }
+
+  const lateIncreaseCounter = () => {
+    setTimeout(() => {
+      setCounter((prevState) => {
+        return prevState + 1
+      })
+    }, 2000)
+  }
+
+  return (
+    <>
+      <h2>Regular Counter</h2>
+      <h1>{counter}</h1>
+      <button className="btn" onClick={decreaseCounter}>
+        Decrease
+      </button>
+      <button className="btn" onClick={resetCounter}>
+        Reset
+      </button>
+      <button className="btn" onClick={increaseCounter}>
+        Increase
+      </button>
+
+      <section style={{ margin: '4rem 0' }}>
+        <h2>More Complex Counter</h2>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={lateIncreaseCounter}>
+          Increase Later
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default UseStateCounter
