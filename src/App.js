de46619edd8e2c93756dc7f71cd00711f13ef364
Subin Sudhakaran-Project SAPPER::Accenture <acc032@GMMCHENNAI.COM>
2020-10-25 16:24:29 +0530

    useEffect cleanup function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import Setup from './tutorial/2-useEffect/final/1-useEffect-basics'
-import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
+// import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
+import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
 function App() {
   return (
     <div className="container">
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size, setSize] = useState(window.innerWidth)
 
-export default UseEffectCleanup;
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+
+    return () => {
+      // console.log('cleanup..')
+      window.removeEventListener('resize', checkSize)
+    }
+  })
+
+  return (
+    <>
+      <h2>useEffect cleanup</h2>
+
+      <h1>{size} px</h1>
+    </>
+  )
+}
+
+export default UseEffectCleanup
