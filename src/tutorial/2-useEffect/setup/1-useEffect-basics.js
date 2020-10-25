a0b544ff33bdc2868a403854f233754a05a574cf
Subin Sudhakaran-Project SAPPER::Accenture <acc032@GMMCHENNAI.COM>
2020-10-25 14:11:30 +0530

    useEffect function basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  const [value, setValue] = useState(0)
+  const handleClick = () => {
+    setValue(value + 1)
+  }
+  useEffect(() => {
+    console.log('useffect running...')
+    if (value > 0) {
+      document.title = `New Messages (${value})`
+    }
 
-export default UseEffectBasics;
+    return () => {}
+  })
+  console.log('rendering...')
+  return (
+    <>
+      <h2>useEffect Basics</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={handleClick}>
+        Click me
+      </button>
+    </>
+  )
+}
+
+export default UseEffectBasics
