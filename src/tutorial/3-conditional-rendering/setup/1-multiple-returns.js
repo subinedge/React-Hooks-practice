f81b079045b1e955940504c650907cfcc93948b8
Subin Sudhakaran-Project SAPPER::Accenture <acc032@GMMCHENNAI.COM>
2020-10-25 23:28:21 +0530

    useEffect toggling return

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,36 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/QuincyLarsons'
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+  const [loading, setLoading] = useState(true)
+  const [error, isError] = useState(false)
+  const [user, setUser] = useState('default user')
 
-export default MultipleReturns;
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json()
+        } else {
+          setLoading(false)
+          isError(true)
+          throw new Error(resp.statusText)
+        }
+      })
+      .then((data) => {
+        setUser(data.login)
+        setLoading(false)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (error) {
+    return <h1>No user found...</h1>
+  }
+  return <h2>{user}</h2>
+}
+
+export default MultipleReturns
